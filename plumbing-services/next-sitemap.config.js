9f99be5801be75726c9478efba5c62b045c526e3
Muhammad Shahzad <147616022+Shahzad512161@users.noreply.github.com>
2026-07-04 17:41:19 +0500

    Update site URL in next-sitemap configuration

--- plumbing-services/next-sitemap.config.js
+++ plumbing-services/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://prideplumbingfl.com',
+  siteUrl: 'https://pride-plumbing-fl.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: [],
